c0244a3f8e531f508a03715d8b8b2c6fff30916a
kcire <erickmagagna84@gmail.com>
2026-05-30 12:56:51 -0300

    refactor: rename VaultInput class to Vault for clarity

---
+++ src/models/Vault.cs
@@ -0,0 +1,8 @@
+﻿namespace CdbCalculator;
+
+public record Vault
+{
+    public required decimal Principal { get; init; }
+    public required int Months { get; init; }
+    public string? Name { get; init; }
+}
--- src/models/VaultInput.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CDB_Calculator;
-
-public class VaultInput
-{
-    public decimal InitialAmount { get; set; }
-    public ushort Months { get; set; }
-    public string? VaultName { get; set; }
-    public decimal MonthlyRate { get; set; } = 0.0085m;
-}
